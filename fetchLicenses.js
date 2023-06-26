46e21a1293067766399c7c8426ba6fceed8700ce
Jesus Santos <jo_nez@hotmail.com>
2023-06-26 07:20:11 -0700

    push for availability while remote

--- fetchLicenses.js
+++ fetchLicenses.js
@@ -2,10 +2,37 @@ const fetch = require('node-fetch');
 
 const licenseUrl = 'https://api.github.com/licenses';
 
-async function getLicenses() {
+// async function getLicenses() {
+//     const response = await fetch(licenseUrl);
+//     const licenseData = await response.json();
+//     console.log(licenseData);
+// }
+
+// getLicenses();
+
+// async function fetchLicenseObjects() {
+//     const response = await fetch(licenseUrl);
+//     const licenseData = await response.json();
+//     const licenses = licenseData.map(obj => obj.spdx_id);
+//     console.log(licenses);
+// }
+
+// fetchLicenseObjects();
+
+const licenseTypes = async function fetchLicenseTypes() {
     const response = await fetch(licenseUrl);
     const licenseData = await response.json();
-    console.log(licenseData);
+    const licenses = licenseData.map(obj => obj.spdx_id);
+    // console.log(licenses);
 }
 
-getLicenses();
+fetchLicenseTypes();
+
+const licenseTypes = async function fetchLicenseTypes() {
+    const response = await fetch(licenseUrl);
+    const licenseData = await response.json();
+    const licenses = licenseData.map(obj => obj.spdx_id);
+    // console.log(licenses);
+}
+
+fetchLicenseTypes();
--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 // TODO: Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
+const fetch = require('node-fetch');
+const { makeBadge, ValidationError } = require('badge-maker');
+
+const licenseUrl = 'https://api.github.com/licenses';
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -8,36 +12,43 @@ const questions = [
         type: 'input',
         name: 'title',
         message: 'Enter your project title:',
+        default: 'TESTING README GENERATIS TITLING',
     },
     {
         type: 'input',
         name: 'description',
         message: 'Provide a short description explaining the what, why, and how of your project:',
+        default: 'TESTING README GENERATIS DESCRIBING',
     },
     {
         type: 'input',
         name: 'installation',
         message: 'Provide instructions on how to install your application:',
+        default: 'TESTING README GENERATIS INSTALLING',
     },
     {
         type: 'input',
         name: 'usage',
         message: 'Provide information on how your application is used:',
+        default: 'TESTING README GENERATIS USING',
     },
     {
         type: 'input',
         name: 'contributing',
         message: 'Provide guidelines on how someone can contribute to your project:',
+        default: 'TESTING README GENERATIS CONTRIBUTING',
     },
     {
         type: 'input',
         name: 'tests',
         message: 'Provide instructions on how to test your application:',
+        default: 'TESTING README GENERATIS TESTING',
     },
     {
         type: 'input',
         name: 'license',
         message: 'Choose the license your application will be released under.',
+        default: 'TESTING README GENERATIS LICENSING',
     },
 ]
 
