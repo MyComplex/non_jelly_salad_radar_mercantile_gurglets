70b809a5f08115d6807e8e24ace141b2daf177ec
Jesus Santos <jo_nez@hotmail.com>
2023-06-27 18:30:59 -0700

    setting up questions for inquirer

--- fetchLicenses.js
+++ fetchLicenses.js
@@ -10,29 +10,35 @@ const licenseUrl = 'https://api.github.com/licenses';
 
 // getLicenses();
 
-// async function fetchLicenseObjects() {
+async function fetchLicenseObjects() {
+    const response = await fetch(licenseUrl);
+    const licenseData = await response.json();
+    const licenses = licenseData.map(obj => obj.spdx_id);
+    console.log(licenses);
+}
+
+fetchLicenseObjects();
+
+// const licenseTypes = async function fetchLicenseTypes() {
 //     const response = await fetch(licenseUrl);
 //     const licenseData = await response.json();
 //     const licenses = licenseData.map(obj => obj.spdx_id);
-//     console.log(licenses);
+//     // console.log(licenses);
 // }
 
-// fetchLicenseObjects();
+// fetchLicenseTypes();
 
-const licenseTypes = async function fetchLicenseTypes() {
-    const response = await fetch(licenseUrl);
-    const licenseData = await response.json();
-    const licenses = licenseData.map(obj => obj.spdx_id);
-    // console.log(licenses);
-}
+// const fetchedLicenses = async () => {
+//     const response = await fetch(licenseUrl);
+//     const licenseData = await response.json();
+//     return licenseData.map(obj => obj.spdx_id);
+// }
 
-fetchLicenseTypes();
+// ;(async () => {
+//     const ltags = await fetchedLicenses()
+//     console.log(ltags)
+// })
 
-const licenseTypes = async function fetchLicenseTypes() {
-    const response = await fetch(licenseUrl);
-    const licenseData = await response.json();
-    const licenses = licenseData.map(obj => obj.spdx_id);
-    // console.log(licenses);
-}
-
-fetchLicenseTypes();
+// const licenseTags = await fetchedLicenses();
+// console.log(licenseTags);
+// console.log(licenseTypes);
--- index.js
+++ index.js
@@ -4,60 +4,75 @@ const inquirer = require('inquirer');
 const fetch = require('node-fetch');
 const { makeBadge, ValidationError } = require('badge-maker');
 
-const licenseUrl = 'https://api.github.com/licenses';
+/* FETCHING LICENSE TAGS FROM GITHUB */
+function fetchLicenseTags(e) {
+    const licenseUrl = 'https://api.github.com/licenses';
+    fetch(licenseUrl)
+        .then(response => response.json())
+        .then(data => data.map(obj => obj.spdx_id))
+        .then(tags => {
+            genQuestions(tags);
+        })
+        .catch(err => {
+            console.log('Error in fetchLicenseTags: ' + err);
+        })
+
+}
 
 // TODO: Create an array of questions for user input
-const questions = [
-    {
-        type: 'input',
-        name: 'title',
-        message: 'Enter your project title:',
-        default: 'TESTING README GENERATIS TITLING',
-    },
-    {
-        type: 'input',
-        name: 'description',
-        message: 'Provide a short description explaining the what, why, and how of your project:',
-        default: 'TESTING README GENERATIS DESCRIBING',
-    },
-    {
-        type: 'input',
-        name: 'installation',
-        message: 'Provide instructions on how to install your application:',
-        default: 'TESTING README GENERATIS INSTALLING',
-    },
-    {
-        type: 'input',
-        name: 'usage',
-        message: 'Provide information on how your application is used:',
-        default: 'TESTING README GENERATIS USING',
-    },
-    {
-        type: 'input',
-        name: 'contributing',
-        message: 'Provide guidelines on how someone can contribute to your project:',
-        default: 'TESTING README GENERATIS CONTRIBUTING',
-    },
-    {
-        type: 'input',
-        name: 'tests',
-        message: 'Provide instructions on how to test your application:',
-        default: 'TESTING README GENERATIS TESTING',
-    },
-    {
-        type: 'input',
-        name: 'license',
-        message: 'Choose the license your application will be released under.',
-        default: 'TESTING README GENERATIS LICENSING',
-    },
-]
+function genQuestions(tags) {
+    const questions = [
+        {
+            type: 'input',
+            name: 'title',
+            message: 'Enter your project title:',
+            default: 'TESTING README GENERATIS TITLING',
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Provide a short description explaining the what, why, and how of your project:',
+            default: 'TESTING README GENERATIS DESCRIBING',
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'Provide instructions on how to install your application:',
+            default: 'TESTING README GENERATIS INSTALLING',
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Provide information on how your application is used:',
+            default: 'TESTING README GENERATIS USING',
+        },
+        {
+            type: 'input',
+            name: 'contributing',
+            message: 'Provide guidelines on how someone can contribute to your project:',
+            default: 'TESTING README GENERATIS CONTRIBUTING',
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'Provide instructions on how to test your application:',
+            default: 'TESTING README GENERATIS TESTING',
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'Choose the license your application will be released under.',
+            default: 'TESTING README GENERATIS LICENSING',
+        },
+    ]
+}
 
 inquirer
-  .prompt(questions)
-  .then((answers) => {
-    // Use user feedback for... whatever!!
-    console.log(answers);
-  })
+    .prompt(questions)
+    .then((answers) => {
+        // Use user feedback for... whatever!!
+        console.log(answers);
+    })
 //   .catch((error) => {
 //     if (error.isTtyError) {
 //       // Prompt couldn't be rendered in the current environment
