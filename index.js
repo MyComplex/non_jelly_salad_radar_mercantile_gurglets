2afbe9f41536ed3cf5bb2cf3155e2c9f8a88b115
Jesus Santos <jo_nez@hotmail.com>
2023-07-09 17:07:43 -0700

    generator is working

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// TODO: Include packages needed for this application
+/* PACKAGE DEPENDENCIES */
 const fs = require('fs');
 const inquirer = require('inquirer');
 const fetch = require('node-fetch');
@@ -9,84 +9,122 @@ fetch(licenseUrl)
     .then(response => response.json())
     .then(data => {
         const licenseTypeArray = data.map(obj => obj.spdx_id);
-// TODO: Create an array of questions for user input
-        const questions = [
-            {
-                type: 'input',
-                name: 'title',
-                message: 'Enter your project title:',
-                default: 'TESTING README GENERATIS TITLING'
-            },
-            {
-                type: 'input',
-                name: 'description',
-                message: 'Provide a short description explaining the what, why, and how of your project:',
-                default: 'TESTING README GENERATIS DESCRIBING'
-            },
-            {
-                type: 'input',
-                name: 'installation',
-                message: 'Provide instructions on how to install your application:',
-                default: 'TESTING README GENERATIS INSTALLING'
-            },
-            {
-                type: 'input',
-                name: 'usage',
-                message: 'Provide information on how your application is used:',
-                default: 'TESTING README GENERATIS USING'
-            },
-            {
-                type: 'input',
-                name: 'contributing',
-                message: 'Provide guidelines on how someone can contribute to your project:',
-                default: 'TESTING README GENERATIS CONTRIBUTING'
-            },
-            {
-                type: 'input',
-                name: 'tests',
-                message: 'Provide instructions on how to test your application:',
-                default: 'TESTING README GENERATIS TESTING'
-            },
-            {
-                type: 'list',
-                name: 'license',
-                message: 'Choose the license your application will be released under.',
-                choices: licenseTypeArray,
-                loop: false
-            },
-            {
-                type: 'input',
-                name: 'github',
-                message: 'Enter your GitHub username.',
-                default: "octocat"
-            },
-            {
-                type: 'input',
-                name: 'email',
-                message: 'Enter your email address.',
-                default: "octocat@github.com"
-            }
-        ];
-
+        /* PROMPT FOR USER INPUT */
         inquirer
-            .prompt(questions)
+            .prompt([
+                {
+                    type: 'input',
+                    name: 'title',
+                    message: 'Enter your project title:',
+                    default: 'Professional README.md Generator'
+                },
+                {
+                    type: 'input',
+                    name: 'description',
+                    message: 'Provide a short description explaining the what, why, and how of your project:',
+                    default: 'This application takes input from the user and generates a professional README.md file with information about the application.'
+                },
+                /* CONFIRM IF SECTION SHOULD BE INCLUDED */
+                // {
+                //     type: 'confirm',
+                //     name: 'hasInstallation',
+                //     message: 'Do yo have installation instructions for your application?',
+                //     default: false
+                // },
+                {
+                    type: 'input',
+                    name: 'installation',
+                    message: 'Provide instructions on how to install your application:',
+                    /* PART OF THE CONFIRMATION LOGIC FROM ABOVE */
+                    // when: answers => answers.hasInstallation,
+                    default: 'Clone the repository, change to the cloned application directory, and run "npm install."'
+                },
+                {
+                    type: 'input',
+                    name: 'usage',
+                    message: 'Provide information on how your application is used:',
+                    default: 'To use this application, enter "node index.js" in your terminal and hit the "Enter" key.'
+                },
+                {
+                    type: 'input',
+                    name: 'contributing',
+                    message: 'Provide guidelines on how someone can contribute to your project:',
+                    default: 'Feel free to fork this repository and create a Pull Request.'
+                },
+                {
+                    type: 'input',
+                    name: 'tests',
+                    message: 'Provide instructions on how to test your application:',
+                    default: 'npm run test'
+                },
+                {
+                    type: 'list',
+                    name: 'license',
+                    message: 'Choose the license your application will be released under.',
+                    choices: licenseTypeArray,
+                    loop: false
+                },
+                {
+                    type: 'input',
+                    name: 'github',
+                    message: 'Enter your GitHub username.',
+                    default: "octocat"
+                },
+                {
+                    type: 'input',
+                    name: 'email',
+                    message: 'Enter your email address.',
+                    default: "octocat@github.com"
+                }
+            ])
             .then((answers) => {
-// Use user feedback for... whatever!!
-                fs.writeFileSync('README.md', `# ${answers.title}
-                
-                ![Static Badge](https://img.shields.io/badge/license-${answers.license}-green)
+                /* REPLACE HYPHENS WITH UNDERSCORES FOR SHIELDS.IO STATIC BADGE URL */
+                const badgeFriendlyLicenseUrl = encodeURI(`https://img.shields.io/badge/license-${answers.license.replace('-', '_')}-green`);
+                /* WRITE TO README.md FILE */
+                fs.writeFileSync('README.md', `
+# ${answers.title}
 
-                ## Description
+![Static Badge](${badgeFriendlyLicenseUrl})
 
-                ${answers.description}
+## Description
 
-                ## Table of Contents
+${answers.description}
 
-                - [Installation] (#${answers.installation})
-                - [Usage] (#${answers.usage})
-                - [Contributing] (#${answers.contributing})
-                - [Testing] (#${answers.tests})
-                - [License] (#${answers.license})
+## Table of Contents
+
+- [Installation](#Installation)
+- [Usage](#Usage)
+- [Contributing](#Contributing)
+- [Testing](#Testing)
+- [License](#License)
+
+## Installation
+
+To install this application, perform the following:
+
+${answers.installation}
+
+## Usage
+
+${answers.usage}
+
+## License
+
+[${answers.license}](./LICENSE) © ${answers.github}
+
+## Contributing
+
+${answers.contributing}
+
+## Test
+
+To test this application, complete the installation steps from above and enter the following in your terminal:
+
+${answers.tests}
+
+## Questions
+
+If you have any questions, please feel free to reach out to me via [email](mailto:${answers.email}) or on [Github](https://github.com/${answers.github}/).
 `);
             });
 
@@ -94,12 +132,3 @@ fetch(licenseUrl)
     .catch(err => {
         console.log('An error occurred: ' + err);
     });
-
-// TODO: Create a function to write README file
-// function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-// function init() {}
-
-// Function call to initialize app
-// init();
